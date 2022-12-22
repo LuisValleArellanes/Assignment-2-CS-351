bc90d867c83293b1e8a455b63134084116e5e9ba
Manasi Patil <24mpatil@csu.fullerton.edu>
2022-12-21 22:11:19 -0800

    Message Queues

--- part1/reciever.c
+++ part1/reciever.c
@@ -9,6 +9,7 @@
 #include <sys/shm.h>
 #include <string.h>
 #include <mqueue.h>
+using namespace std;
 
 
 void recvFile(int sigNum){
@@ -48,8 +49,18 @@ void recvFile(int sigNum){
 }
 
 void messageQueue() {
-  mqd_t qid = mq_open("/cpsc351sharedmem", O_CREAT, 0777, &attr)
-  int recvMQ = mq_receive(qid, char buff[100], 4096, 10)
+
+  mq_attr attr;
+
+  attr.mq_flags = 0;
+  attr.mq_maxmsg = 10;
+  attr.mq_msgsize = 4096;
+  attr.mq_curmsg = 0
+
+  mqd_t qid = mq_open("/cpsc351sharedmem", O_CREAT | O_RDWR, 0744, &attr);
+  int recvMQ = mq_receive(qid, char buff[100], 4096, NULL);
+
+
 }
 
 int main(){
